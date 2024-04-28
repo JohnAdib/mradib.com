cd710aa6893e6758c69186e1de48d955235fdddd
MrAdib <Mr.John.Adib@gmail.com>
2024-04-29 00:53:56 +0100

    update not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,21 +3,25 @@ import { Container } from '@/components/container';
 
 export default function NotFound() {
   return (
-    <Container className="flex h-full items-center pt-16 sm:pt-32">
-      <div className="flex flex-col items-center">
-        <p className="text-base font-semibold text-zinc-400 dark:text-zinc-500">
+    <div
+      className="flex w-full flex-col"
+    >
+      <Container className="flex h-full items-center pt-16 sm:pt-32">
+        <div className="flex flex-col items-center">
+          <p className="text-base font-semibold text-zinc-400 dark:text-zinc-500">
           404
-        </p>
-        <h1 className="mt-4 text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-          صفحه یافت نشد
-        </h1>
-        <p className="mt-4 text-base text-zinc-600 dark:text-zinc-400">
-          متاسفانه صفحه‌ای که به دنبال آن بودید وجود ندارد.
-        </p>
-        <Button href="/" variant="secondary" className="mt-4">
-          برو به صفحه نخست
-        </Button>
-      </div>
-    </Container>
+          </p>
+          <h1 className="mt-4 text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
+          Page Not Found
+          </h1>
+          <p className="mt-4 text-base text-zinc-600 dark:text-zinc-400">
+          Unfortunately the page you were looking for could not be found.
+          </p>
+          <Button href="/" variant="secondary" className="mt-4">
+          Go to Homepage
+          </Button>
+        </div>
+      </Container>
+    </div>
   );
 }
