ec664423d5d24650f0f7b34e4b65fdee8bdd82b0
MrAdib <Mr.John.Adib@gmail.com>
2024-05-12 20:39:02 +0100

    fix sample

--- src/components/faq/faq-sample-qa.ts
+++ src/components/faq/faq-sample-qa.ts
@@ -2,8 +2,8 @@ import { IFaqQA } from './faq-interface';
 
 export const sampleQA: IFaqQA[] = [
   {
-    question: 'ساخت رزومه با هوش مصنوعی چطوریه؟',
-    answer:
-          'شما برای نوشتن',
+    id: 'aa',
+    q: 'Sample Question?',
+    a: 'Sample Answer',
   },
 ];
