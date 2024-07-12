b4229aab4ada239f64c99f87a7faf0e7c88d137c
MrAdib <Mr.John.Adib@gmail.com>
2024-07-12 14:08:39 +0100

    add redirect to remove the trailing slash

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,23 @@
+'use client';
+
 import { Page404 } from '@/components/page-404/page-404';
+import { redirect, usePathname } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function NotFound() {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (pathname.endsWith('/')) {
+      const urlWithoutTrailingSlash = pathname.slice(0, -1);
+
+      if (urlWithoutTrailingSlash.length > 0) {
+        console.warn('URL ends with a slash. redirecting... from ', pathname, ' to ', urlWithoutTrailingSlash);
+        redirect(urlWithoutTrailingSlash);
+      }
+    }
+  }, [pathname]);
+
   return (
     <>
       <Page404 />
