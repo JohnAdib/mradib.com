e0932186f6c844e17d551af99c224f3b892f5a55
MrAdib <mr.john.adib@gmail.com>
2024-11-06 11:31:40 +0000

    add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,8 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import prettierConfigRecommended from "eslint-plugin-prettier/recommended";
 import pluginReact from "eslint-plugin-react";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 
 /** @type {import('eslint').Linter.Config[]} */
@@ -11,4 +12,5 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  prettierConfigRecommended,
 ];
