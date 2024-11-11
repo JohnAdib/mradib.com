861cd754d4783a32f36fee76415f6edd52eed5e3
MrAdib <mr.john.adib@gmail.com>
2024-11-11 11:11:58 +0000

    feat: add CORS headers to sitemap response

--- src/app/sitemap/fa/route.ts
+++ src/app/sitemap/fa/route.ts
@@ -6,6 +6,10 @@ export const dynamic = "force-static";
 export async function GET() {
   return new Response(generateSitemapXml(sitemapListFa), {
     headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+
       "Content-Type": "text/xml",
     },
   });
