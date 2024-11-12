f75a2745ef2cbba3a7963974a6450d3e9e603bd8
MrAdib <mr.john.adib@gmail.com>
2024-11-12 16:01:27 +0000

    refactor: adjust positioning of Card.Eyebrow decoration in card component

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -106,13 +106,13 @@ Card.Eyebrow = function CardEyebrow<T extends React.ElementType = "p">({
       className={clsx(
         className,
         "relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400 dark:text-zinc-500",
-        decorate && "pr-3.5",
+        decorate && "pl-3.5",
       )}
       {...props}
     >
       {decorate && (
         <span
-          className="absolute inset-y-0 right-0 flex items-center"
+          className="absolute inset-y-0 left-0 flex items-center"
           aria-hidden="true"
         >
           <span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500" />
