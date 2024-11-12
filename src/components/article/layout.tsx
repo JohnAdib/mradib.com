6e072cab1ddd4160978374de00cbe8172500dd7a
MrAdib <mr.john.adib@gmail.com>
2024-11-12 16:07:44 +0000

    fix back btn position for ltr

--- src/components/article/layout.tsx
+++ src/components/article/layout.tsx
@@ -46,7 +46,24 @@ export function ArticleLayout({
               type="button"
               onClick={() => router.back()}
               aria-label="Go back to the previous page"
-              className="group mb-4 flex h-10 w-10 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 transition lg:absolute lg:-right-5 lg:-mt-2 lg:mb-0 xl:-top-1.5 xl:right-0 xl:mt-0 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20 select-none"
+              className={clsx(
+                "backBtn",
+                "group mb-4",
+                "flex items-center justify-center h-10 w-10",
+                "rounded-full",
+                "bg-white",
+                "transition",
+                "shadow-md shadow-zinc-800/5",
+                "ring-1 ring-zinc-900/5",
+                "lg:absolute",
+                "rtl:lg:-right-5",
+                "lg:-left-5",
+                "lg:-mt-2 lg:mb-0",
+                "xl:-top-1.5 xl:right-0 xl:mt-0",
+                "dark:border dark:border-zinc-700/50 dark:bg-zinc-800 ",
+                "dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20",
+                "select-none",
+              )}
             >
               <ArrowRightIcon className="h-4 w-4 stroke-zinc-500 transition group-hover:stroke-zinc-700 dark:stroke-zinc-500 dark:group-hover:stroke-zinc-400" />
             </button>
