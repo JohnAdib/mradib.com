59fdf3c7f80ad9355a689054444a1c87cf9b3135
John Adib <mr.john.adib@gmail.com>
2025-03-01 00:40:54 +0000

    feat: Add ids to sample todo list items

--- src/components/todo/sample-data.ts
+++ src/components/todo/sample-data.ts
@@ -2,22 +2,27 @@ import type { ITodo } from "./interface";
 
 export const todoSampleData: ITodo[] = [
 	{
+		id: "garden",
 		item: "Weed front garden",
 		completed: true,
 	},
 	{
+		id: "old-toby",
 		item: "Chill and smoke some Old Toby",
 		completed: true,
 	},
 	{
+		id: "ring-secret",
 		item: "Keep ring secret and safe",
 		completed: false,
 	},
 	{
+		id: "gandalf",
 		item: "Meet Gandalf at Bree",
 		completed: false,
 	},
 	{
+		id: "destroy-ring",
 		item: "Destroy ring and defeat dark lord",
 		completed: false,
 	},
