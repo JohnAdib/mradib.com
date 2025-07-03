25988dbf1bc417d02611ff26f670525493f52a3e
John Adib <mr.john.adib@gmail.com>
2025-07-03 11:08:07 +0100

    chore: Upgrade Biome to version 2.0.6 and update configuration for file inclusion and linter rules

--- src/app/(en)/(articles)/articles-list.ts
+++ src/app/(en)/(articles)/articles-list.ts
@@ -1,4 +1,5 @@
+import { article as designSystemsAi } from "./design-systems-ai-and-the-art-of-separation-of-concern/page";
 import { article as githubAutoink } from "./github-autolink/page";
 import type { IArticle } from "./i-article-interface";
 
-export const articlesList: IArticle[] = [githubAutoink];
+export const articlesList: IArticle[] = [designSystemsAi, githubAutoink];
